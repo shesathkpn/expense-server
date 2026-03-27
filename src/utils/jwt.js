b49a865f26ec0dc336383d6cd42b018f815ee826
shesathkpn <kpnshesath@gmail.com>
2026-03-27 17:29:19 +0530

    jwt.js

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -24,13 +24,13 @@ const setCookies = (res, accessToken, refreshToken) => {
   res.cookie('access_token', accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: 'none',
     maxAge: 15 * 60 * 1000, // 15 minutes
   });
   res.cookie('refresh_token', refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
